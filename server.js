596bed8a6acf0d262618afbc066d4516dd880629
CleristonBernardes <cleristonbernardes@yahoo.com.br>
2016-07-11 00:57:56 +1000

    filtering
    filtering and function to handle response

--- server.js
+++ server.js
@@ -6,27 +6,28 @@ var fs = require("fs");
 
 var app = express();
 app.use(bodyParser.json());
+app.use(function(err, req, res, next) {
+  handleResponse(res, 200, {"error" : "Unexpected exception"});
+})
 
 app.get('/sourceData', function (req, res) {
-	console.log(global.data_path);
-   fs.readFile( global.data_path + "payload.json", 'utf8', function (err, data) {
-       res.end( data );
-   });
+	fs.readFile( global.data_path + "payload.json", 'utf8', function (err, data) {
+	   handleResponse(res, 200, data);
+	});
 })
 
 app.post('/filteredData', function (req, res) {
-	//console.log(req.body);
-    //res.end("ok");
-	var result = business.FilterFullPayLoad(req.body);
-	res.end(result);
+	var result = business.filterFullPayLoad(req.body);
+	console.log(result.exchange);
+	handleResponse(res, result.status, result.exchange);
 })
 
 
 var server = app.listen(8081, function () {
-
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log("Example app listening at http://%s:%s", host, port)
-
+  console.log("Service running....");
 })
+
+
+function handleResponse(res, code, exchange) {
+  res.status(code || 500).json(exchange);
+}
